24adc0414f878ba6d66a5e68fded573eb7028cd0
mandrade <mandrade@bempromotora.com.br>
2019-08-27 22:55:15 -0300

    Adicionado camada de Comunicação

---
+++ Comunicacao/ConexaoBanco/Implementacao/ConexaoBanco.cs
@@ -0,0 +1,41 @@
+﻿using Comunicacao.ConexaoBanco.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Comunicacao.ConexaoBanco.Implementacao
+{
+    public class ConexaoBanco : IConexaoBanco
+    {
+        public List<T> Consultar<T>(string consulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Consultar<T>(string consulta, T parametros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbConnection CriarNovaConexao()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Executar(string consulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Executar<T>(string consulta, T parametros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ObterConsultaArquivoSQL(string nomeArquivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Comunicacao/ConexaoBanco/Interface/IConexaoBanco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Comunicacao.ConexaoBanco.Interface
+{
+   public interface IConexaoBanco
+    {
+        string ObterConsultaArquivoSQL(string nomeArquivo);
+        IDbConnection CriarNovaConexao();
+        List<T> Consultar<T>(string consulta);
+        List<T> Consultar<T>(string consulta, T parametros);
+        int Executar(string consulta);
+        int Executar<T>(string consulta, T parametros);
+    }
+}
