42e17b30970b450d2535aa6b3e56b4bc1eb2a544
mandrade <mandrade@bempromotora.com.br>
2019-08-27 22:45:01 -0300

    Adicionado camada de dominio

---
+++ Dominio/Usuario/Interface/IUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Usuario.Interface
+{
+    public interface IUsuario
+    {
+        int IdUsuario { get; set; }
+        string Nome { get; set; }
+        string Apelido { get; set; }
+        string Senha { get; set; }
+        string Grupo { get; set; }
+        DateTime DataCriacao { get; set; }
+        byte UsuarioAtivo { get; set; }
+        string Cpf { get; set; }
+    }
+}
