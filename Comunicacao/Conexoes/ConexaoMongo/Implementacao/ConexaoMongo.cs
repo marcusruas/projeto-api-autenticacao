acb5abccde5c1dee964a1442bf2c1cc8638591e0
mandrade <mandrade@bempromotora.com.br>
2019-09-25 22:33:40 -0300

    Adicionado método para ler arquivos JSON de consulta ao mongo

---
+++ Comunicacao/Conexoes/ConexaoMongo/Implementacao/ConexaoMongo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comunicacao.Conexoes.ConexaoMongo.Implementacao
+{
+    public class ConexaoMongo
+    {
+    }
+}
--- Comunicacao/Conexoes/LeitorArquivos.cs
+++ Comunicacao/Conexoes/LeitorArquivos.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Comunicacao.Conexoes.ConexaoSQL;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -39,6 +40,37 @@ namespace Comunicacao.Conexoes
             catch (Exception)
             {
                 throw new Exception("Ocorreu um erro ao ler o arquivo SQL indicado.");
+            }
+            return conteudoArquivo;
+        }
+
+        public static string LerArquivoJSON(string nomeArquivo)
+        {
+            string pathApi = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            string conteudoArquivo = string.Empty;
+
+            try
+            {
+                string[] projetoArquivo = nomeArquivo.Split('/');
+                string pathArquivo = Path.Combine(pathApi, projetoArquivo[0], "JSON", $"{projetoArquivo[1]}.json");
+                pathArquivo = pathArquivo.Replace("file:\\", "");
+
+                string[] linhas;
+
+                if (!File.Exists(pathArquivo))
+                    throw new Exception("Não foi possível localizar o arquivo de consulta ao banco com este nome.");
+
+                linhas = File.ReadAllLines(pathArquivo);
+                foreach (string linha in linhas)
+                    conteudoArquivo += (linha + " ");
+            }
+            catch (ArgumentNullException)
+            {
+                throw new Exception("O arquivo de consulta ao banco de dados está vazio.");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocorreu um erro ao ler o arquivo JSON indicado.");
             }
             return conteudoArquivo;
         }
