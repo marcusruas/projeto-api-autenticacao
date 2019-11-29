55571134d1909da946ddd68b63458ec60fe00db1
mandrade <mandrade@bempromotora.com.br>
2019-11-28 22:38:18 -0300

    Corrigido implementação da ConexaoDB e removido health checks

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,48 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Aplicacao;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using HealthChecks.UI.Client;
-using System.IO;
-using System.Reflection;
-using IoC;
-using Aplicacao;
-using AutoMapper;
 
 namespace Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             //Swagger
-            services.AddSwaggerGen(c =>
-            {
+            services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v2", new Info { Title = "PadraoAPI", Version = "v2" });
             });
-
-            //HealthChecks
-            services.AddHealthChecks();
-            services.AddHealthChecksUI();
 
             //Cors
             services.AddCors(options => {
@@ -65,30 +47,18 @@ namespace Api
             services.AddScoped<IMapper>(x => new Mapper(mapeamentoDominio));
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            } else {
                 app.UseHsts();
             }
 
             //Swagger
             app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
+            app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v2/swagger.json", "PadraoAPI V2");
             });
-
-            //HealthChecks
-            app.UseHealthChecks("/health/json", new HealthCheckOptions() {
-                Predicate = _ => true,
-                ResponseWriter= UIResponseWriter.WriteHealthCheckUIResponse
-            });
-            app.UseHealthChecksUI(config => config.UIPath = "/health");
 
             //Cors
             app.UseCors("PermissionamentoReact");
