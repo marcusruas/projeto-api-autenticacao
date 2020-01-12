f0f08d8f7abfaa14e3236e7516305395581760d2
mandrade <mandrade@bempromotora.com.br>
2020-01-11 22:09:01 -0300

    Adicionado Camada de helpers.

---
+++ Helpers/CpfFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace Helpers
+{
+    public class CpfFormat
+    {
+        public static string FormatarCpf(string cpf) {
+            var cpfFormatado = string.Empty;
+
+            for (int i = 0; i < cpf.Length; i++) {
+                cpfFormatado += cpf[i];
+
+                if (i == 2 || i == 5)
+                    cpfFormatado += '.';
+
+                if (i == 8)
+                    cpfFormatado += '-';
+            }
+
+            return cpfFormatado;
+        }
+    }
+}
