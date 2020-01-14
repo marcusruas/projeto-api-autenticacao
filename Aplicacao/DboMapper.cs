183923ea002766e624c6facf9b5c398bac00526f
mandrade <mandrade@bempromotora.com.br>
2020-01-13 22:58:21 -0300

    Criação de classes responsáveis pelo mapeamento de objetos de banco.

---
+++ Aplicacao/DboMapper.cs
@@ -0,0 +1,30 @@
+﻿using Aplicacao.Grupo;
+using AutoMapper;
+using Dapper.FluentMap.Mapping;
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Aplicacao
+{
+    public class DboMapper<T> : EntityMap<T> where T : class
+    {
+        public DboMapper() {
+            CriarMapeamento();
+        }
+
+        private void CriarMapeamento() {
+            Type classe = typeof(T);
+            foreach (var prop in classe.GetProperties()) {
+                var descricao = ObterDescription(prop);
+                Map(p => prop)
+                .ToColumn(descricao);
+            }
+        }
+
+        private string ObterDescription(PropertyInfo prop) {
+            var description = prop.GetCustomAttribute<DescriptionAttribute>();
+            return description == null ? prop.Name : description.Description;
+        }
+    }
+}
---
+++ Aplicacao/Mapeamentos.cs
@@ -0,0 +1,13 @@
+﻿using Aplicacao.Grupo;
+using Dapper.FluentMap;
+using Dapper.FluentMap.Configuration;
+
+namespace Aplicacao
+{
+    public static class Mapeamentos
+    {
+        public static void MapearObjetosBanco(this FluentMapConfiguration configuracoes) {
+            configuracoes.AddMap(new DboMapper<GrupoDbo>());
+        }
+    }
+}
