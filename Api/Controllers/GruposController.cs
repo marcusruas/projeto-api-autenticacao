ff342b16233ef503c6e313d921878a1c9363dc26
mandrade <mandrade@bempromotora.com.br>
2020-01-15 23:04:06 -0300

    Criado controller de grupo

---
+++ Api/Controllers/GruposController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MandradePkgs.Retornos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servico.Grupo.Interface;
+
+namespace Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class GruposController : ControllerApi
+    {
+        private IGrupoSrv _servico { get; }
+        private IMapper _mapper { get; }
+
+        public GruposController(IGrupoSrv servico, IMapper mapper) {
+            _servico = servico;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public RespostaApi IncluirNovoGrupo(string nome, int nivel) {
+            return RespostaPadrao(_servico.InserirNovoUsuario(nome, nivel));
+        }
+    }
+}
