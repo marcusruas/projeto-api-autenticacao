3fc61a4a45c5c0ec951ed50ae371c5d23847be60
mandrade <mandrade@bempromotora.com.br>
2020-01-17 22:17:39 -0300

    Adicionado método de serviço para o novo endpoint

--- Servico/Grupo/Implementacao/GrupoSrv.cs
+++ Servico/Grupo/Implementacao/GrupoSrv.cs
@@ -5,6 +5,9 @@ using Servico.Grupo.Interface;
 using MandradePkgs.Retornos.Erros.Exceptions;
 using Aplicacao.Grupo;
 using MandradePkgs.Mensagens;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Servico.Grupo.Implementacao
 {
@@ -20,19 +23,41 @@ namespace Servico.Grupo.Implementacao
         }
 
         public bool InserirNovoUsuario(string nome, string descricao, NivelGrupo nivel) {
-            var dominio = new GrupoDom(nome, descricao, nivel);
-            if (!dominio.NomeValido())
-                throw new RegraNegocioException("Nome do grupo deve conter mais de 5 caractéres e não possuir números.");
-            var grupo = _mapper.Map<GrupoDbo>(dominio);
+            try {
+                var dominio = new GrupoDom(nome, descricao, nivel);
+                if (!dominio.NomeValido())
+                    throw new RegraNegocioException("Nome do grupo deve conter mais de 5 caractéres e não possuir números.");
+                var grupo = _mapper.Map<GrupoDbo>(dominio);
 
-            var sucesso = _repositorio.AdicionarGrupo(grupo);
+                var sucesso = _repositorio.AdicionarGrupo(grupo);
 
-            if (!sucesso)
-                throw new RegraNegocioException("Já existe um grupo registrado com este nome.");
+                if (!sucesso)
+                    throw new RegraNegocioException("Já existe um grupo registrado com este nome.");
 
-            _mensagens.AdicionarMensagem("Grupo adicionado com sucesso!");
+                _mensagens.AdicionarMensagem("Grupo adicionado com sucesso!");
 
-            return sucesso;
+                return sucesso;
+            }catch(Exception ex) {
+                _mensagens.AdicionarMensagem(TipoMensagem.Erro, ex.Message);
+                throw new FalhaExecucaoException(ex.Message);
+            }
+        }
+
+        public List<GrupoDom> GruposPorNivel(int nivel) {
+            try { 
+                bool nivelValido = Enum.GetValues(typeof(NivelGrupo)).Cast<int>()
+                    .Any(grupo => grupo == nivel);
+
+                if (!nivelValido)
+                    throw new RegraNegocioException("Nível informado não existe. Favor selecionar outro.");
+
+                var listaGrupos = _repositorio.ObterGruposPorNivel(nivel);
+
+                return _mapper.Map<List<GrupoDom>>(listaGrupos);
+            } catch (Exception ex) {
+                _mensagens.AdicionarMensagem(TipoMensagem.Erro, ex.Message);
+                throw new FalhaExecucaoException(ex.Message);
+            }
         }
     }
 }
--- Servico/Grupo/Interface/IGrupoSrv.cs
+++ Servico/Grupo/Interface/IGrupoSrv.cs
@@ -1,9 +1,11 @@
 ﻿using Dominio.Grupo;
+using System.Collections.Generic;
 
 namespace Servico.Grupo.Interface
 {
     public interface IGrupoSrv
     {
         bool InserirNovoUsuario(string nome, string descricao, NivelGrupo nivel);
+        List<GrupoDom> GruposPorNivel(int nivel);
     }
 }
