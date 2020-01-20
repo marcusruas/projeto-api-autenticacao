5b518d348e11fc5f92331781e0c3d7bb44070667
mandrade <mandrade@bempromotora.com.br>
2020-01-19 22:32:37 -0300

    Adicionado repositorio de pessoas

---
+++ Repositorio/Pessoa/Implementacao/PessoaRep.cs
@@ -0,0 +1,23 @@
+﻿using Aplicacao.Pessoa;
+using Dapper;
+using MandradePkgs.Conexoes;
+using static MandradePkgs.Conexoes.Mapeamentos.DboSqlMapper;
+using Repositorio.Pessoa.Interface;
+
+namespace Repositorio.Pessoa.Implementacao
+{
+    public class PessoaRep : IPessoaRep
+    {
+        private IConexaoSQL _conexao { get; }
+
+        public PessoaRep(IConexaoSQL conexao) {
+            _conexao = conexao;
+        }
+
+        public bool InserirPessoa(PessoaDbo pessoa) {
+            var (comando, conexao) = _conexao.ObterComandoSQLParaBanco(GetType(), "insertPessoa", "SHAREDB");
+            var parametros = MapearParaDbo(pessoa);
+            return conexao.Execute(comando, parametros) == 1;
+        }
+    }
+}
---
+++ Repositorio/Pessoa/Interface/IPessoaRep.cs
@@ -0,0 +1,12 @@
+﻿using Aplicacao.Pessoa;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Pessoa.Interface
+{
+    public interface IPessoaRep
+    {
+        bool InserirPessoa(PessoaDbo pessoa);
+    }
+}
