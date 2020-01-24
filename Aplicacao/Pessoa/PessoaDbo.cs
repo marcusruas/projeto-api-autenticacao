c392a614ad0af33c180b1ee6adcb05bf60e01148
mandrade <mandrade@bempromotora.com.br>
2020-01-23 23:17:11 -0300

    Adicionado descriptions para classe de pessoa.

--- Aplicacao/Pessoa/PessoaDbo.cs
+++ Aplicacao/Pessoa/PessoaDbo.cs
@@ -10,11 +10,15 @@ namespace Aplicacao.Pessoa
     {
         [Description("ID_PESSOA")]
         public int Id { get; set; }
+        [Description("NOME")]
         [StringLength(80)]
         public string Nome { get; set; }
+        [Description("CPF")]
         public long Cpf { get; set; }
+        [Description("EMAIL")]
         [StringLength(100)]
         public string Email { get; set; }
+        [Description("TELEFONE")]
         public long Telefone { get; set; }
     }
 }
