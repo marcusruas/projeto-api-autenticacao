8e9150a836e76b9d9bd41c0e439d3dc53e960f12
marcusruas <marcus.ruas@hotmail.com>
2020-01-29 23:47:27 -0300

    Definido padronização de testes.

--- AutenticacaoApi.Testes/Dominio/Grupo.cs
+++ AutenticacaoApi.Testes/Dominio/Grupo.cs
@@ -13,38 +13,38 @@ namespace AutenticacaoApi.Testes.Dominio
     {
         private IMensagensApi _mensagens { get; }
         public Grupo() {
-            //Mock da mensageria
             _mensagens = Mock.Of<MensagensApi>();
         }
 
         [Fact]
-        public void ValidacaoNomeCorreto() {
+        public void ValidacaoNomeGrupoValido() {
             var dominio = new GrupoDom("Teste", NivelGrupo.Geral, _mensagens);
+            
             dominio.ValidarNome();
 
             _mensagens.Mensagens.ShouldBeEmpty();
         }
 
         [Fact]
-        public void ValidacaoNomeNumeros() {
+        public void ValidacaoNomeGrupoNumeros() {
             string mensagemErro = "Nome do grupo não pode conter números";
             var dominio = new GrupoDom("teste123", NivelGrupo.Geral, _mensagens);
+            
             dominio.ValidarNome();
 
             var validacao = _mensagens.Mensagens.Any(m => m.Texto == mensagemErro);
             validacao &= !_mensagens.Mensagens.Any(m => m.Texto != mensagemErro);
-
             validacao.ShouldBeTrue();
         }
 
         [Fact]
-        public void ValidacaoNivelJustificativa() {
+        public void ValidacaoNivelSemJustificativa() {
             string mensagemErro = "Grupos cadastrados com nivel acima de Gerente devem possuir justificativa.";
             var dominio = new GrupoDom("Teste", NivelGrupo.Administrador, null, _mensagens);
+            
             dominio.ValidarJustificativaNivel();
 
             var validacao = _mensagens.Mensagens.Any(m => m.Texto == mensagemErro);
-
             validacao.ShouldBeTrue();
         }
     }
