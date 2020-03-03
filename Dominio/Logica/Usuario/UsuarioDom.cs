6cf85de75b1b02566a1df19a21c251f5f1c55892
marcusruas <marcus.ruas@hotmail.com>
2020-03-02 23:28:57 -0300

    Correção da criptgrafia de senhas.

--- Dominio/Logica/Usuario/UsuarioDom.cs
+++ Dominio/Logica/Usuario/UsuarioDom.cs
@@ -1,44 +1,87 @@
 ﻿using System;
 using System.Linq;
+using Dominio.ObjetosValor.Formatos;
+using MandradePkgs.Mensagens;
 
 namespace Dominio.Logica.Usuario
 {
     public class UsuarioDom
     {
-        public UsuarioDom(int id, string usuario, string senha, DateTime dataCriacao, bool ativo, GrupoDom grupo, PessoaDom pessoa)
+        public UsuarioDom(int id, string usuario, string senha, DateTime dataCriacao, bool ativo, GrupoDom grupo, PessoaDom pessoa, IMensagensApi mensagens)
         {
             Id = id;
             Usuario = usuario;
-            Senha = senha;
+            Senha = new Senha(senha);
             DataCriacao = dataCriacao;
             Ativo = ativo;
             Grupo = grupo;
             Pessoa = pessoa;
+            _mensagens = mensagens;
         }
 
-        public UsuarioDom(int id, string usuario, string senha, GrupoDom grupo, PessoaDom pessoa)
+        public UsuarioDom(int id, string usuario, string senha, GrupoDom grupo, PessoaDom pessoa, IMensagensApi mensagens)
         {
             Id = id;
             Usuario = usuario;
-            Senha = senha;
+            Senha = new Senha(senha);
             Grupo = grupo;
             Pessoa = pessoa;
             DataCriacao = DateTime.Now;
             Ativo = true;
+            _mensagens = mensagens;
         }
 
         public int Id { get; }
         public string Usuario { get; }
-        public string Senha { get; }
+        public Senha Senha { get; }
         public DateTime DataCriacao { get; }
         public bool Ativo { get; }
         public GrupoDom Grupo { get; }
         public PessoaDom Pessoa { get; }
+        private IMensagensApi _mensagens { get; }
 
-        public bool UsuarioValido() =>
-            char.IsUpper(Usuario[0]) && Usuario.Length > 4;
+        public void ValidarDados()
+        {
+            ValidarUsuario();
+            ValidarSenha();
+            Grupo.ValidarDados();
+            Pessoa.ValidarDados();
+        }
 
-        public bool SenhaValida() =>
-            char.IsUpper(Senha[0]) && Senha.Length >= 8 && Senha.Count(c => char.IsNumber(c)) >= 3;
+        public void ValidarUsuario()
+        {
+            if (string.IsNullOrWhiteSpace(Usuario))
+            {
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Usuário é obrigatório");
+                return;
+            }
+
+            if (!char.IsUpper(Usuario[0]))
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Usuário deve iniciar com letra maiúscula");
+
+            if (Usuario.Length < 5)
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Usuário deve conter mais de 5 caractéres");
+        }
+
+        public void ValidarSenha()
+        {
+            if (Senha.SenhaNula())
+            {
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Senha do usuário é obrigatória");
+                return;
+            }
+
+            if (!Senha.PossuiCaracteresNecessarios())
+                _mensagens.AdicionarMensagem(
+                    TipoMensagem.FalhaValidacao,
+                    "Senha do usuário deve conter ao menos uma letra maiúscula, um número e um caractere especial"
+                );
+
+            if (!Senha.PossuiMinimoCaracteres())
+                _mensagens.AdicionarMensagem(
+                    TipoMensagem.FalhaValidacao,
+                    $"Senha deve conter mais de {Senha.MinimoCaracteresNecessarios} caractéres"
+                );
+        }
     }
 }
