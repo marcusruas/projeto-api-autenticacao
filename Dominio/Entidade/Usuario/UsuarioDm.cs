5bd124a6f52be86f6abcbc801e93b6bccff905ef
marcusruas <marcus.ruas@hotmail.com>
2020-12-23 00:29:04 -0300

    Adicionado dominio

---
+++ Dominio/Entidade/Usuario/UsuarioDm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using Dominio.ObjetoValor.Formatos;
+using MandradePkgs.Mensagens;
+
+namespace Dominio.Entidade.Usuario
+{
+    public class UsuarioDm
+    {
+        public UsuarioDm(int id, string usuario, string senha, DateTime dataCriacao, bool ativo, GrupoDm grupo, PessoaDm pessoa)
+        {
+            Id = id;
+            Usuario = usuario;
+            Senha = new Senha(senha);
+            DataCriacao = dataCriacao;
+            Ativo = ativo;
+            Grupo = grupo;
+            Pessoa = pessoa;
+        }
+
+        public UsuarioDm(int id, string usuario, string senha, GrupoDm grupo, PessoaDm pessoa)
+        {
+            Id = id;
+            Usuario = usuario;
+            Senha = new Senha(senha);
+            Grupo = grupo;
+            Pessoa = pessoa;
+            DataCriacao = DateTime.Now;
+            Ativo = true;
+        }
+
+        public int Id { get; }
+        public string Usuario { get; }
+        public Senha Senha { get; }
+        public DateTime DataCriacao { get; }
+        public bool Ativo { get; }
+        public GrupoDm Grupo { get; }
+        public PessoaDm Pessoa { get; }
+        private IMensagensApi mensagens { get; set; }
+        public IMensagensApi _mensagens
+        {
+            get { return mensagens; }
+            private set { DefinirMensagens(value); }
+        }
+
+        public void DefinirMensagens(IMensagensApi mensagens)
+        {
+            if (this._mensagens == null)
+                this.mensagens = mensagens;
+            else
+                throw new ArgumentException("Não é possível sobrescrever a mensageria do objeto");
+        }
+
+        public void ValidarDados()
+        {
+            ValidarUsuario();
+            ValidarSenha();
+            Grupo.ValidarDados();
+            Pessoa.ValidarDados();
+        }
+
+        public void ValidarUsuario()
+        {
+            if (string.IsNullOrWhiteSpace(Usuario))
+            {
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Usuário é obrigatório");
+                return;
+            }
+
+            if (!char.IsUpper(Usuario[0]))
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Usuário deve iniciar com letra maiúscula");
+
+            if (Usuario.Length < 5)
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Usuário deve conter mais de 5 caractéres");
+        }
+
+        public void ValidarSenha()
+        {
+            if (Senha.SenhaNula())
+            {
+                _mensagens.AdicionarMensagem(TipoMensagem.FalhaValidacao, "Senha do usuário é obrigatória");
+                return;
+            }
+
+            if (!Senha.PossuiCaracteresNecessarios())
+                _mensagens.AdicionarMensagem(
+                    TipoMensagem.FalhaValidacao,
+                    "Senha do usuário deve conter ao menos uma letra maiúscula, um número e um caractere especial"
+                );
+
+            if (!Senha.PossuiMinimoCaracteres())
+                _mensagens.AdicionarMensagem(
+                    TipoMensagem.FalhaValidacao,
+                    $"Senha deve conter mais de {Senha.MinimoCaracteresNecessarios} caractéres"
+                );
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,16 @@
 
 ### Sobre o projeto
 Api criada com o framework .NET Core na versão 3.1, utilizando pacotes criados com arquitetura .NET Core 2.1. 
+
+### Sufixos - Resumo
+    * Representações de objetos
+        * Regras de negócio do objeto - Dm
+        * Representação do objeto no banco de dados (objeto de persistência) - Dpo (Data Persistence Object)
+        * Representação do objeto para entrada/saida de dados da Api - Dto (Data Transfer Object)
+        * Objetos de Valor - N/A (Motivo: objeto de valor não necesitam de abstrações)
+
+    * Serviços
+        * Camada de Serviço - Sv
+        * Camada de Repositorio - Rp
+        * Camada de Comunicacao - Cm
+        * Camada de Api - Controller
